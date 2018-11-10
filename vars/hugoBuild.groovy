e3ea6108aa17f61e7c504cb13167198fc6b66170
Mithun <m.singh2005@gmail.com>
2018-11-10 11:19:28 -0600

    Update hugoBuild.groovy

--- vars/hugoBuild.groovy
+++ vars/hugoBuild.groovy
@@ -21,12 +21,13 @@ def call(body) {
 	  {
    		agent any
    		stages 
-		  {
-      		stage('Source') 
-			  {
-        		git branch: 'test', url: 'git@diyvb:repos/gradle-greetings'
-         		stash name: 'test-sources', includes: 'build.gradle,/src/test'
-      		  }
+		{
+		   stage('Source') 
+		{
+			steps{
+				scm checkout
+			}	 
+		}
       		stage('Build') 
 			{
          
